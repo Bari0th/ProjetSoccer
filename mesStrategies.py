e660b7464dd62d163f49a0b59bf9d8a88e11b99e
Barioth <bernier.teo@gmail.com>
2019-01-27 19:55:30 +0100

    up : le goal peut tirer, ajout de fonction non complétée

--- mesStrategies.py
+++ mesStrategies.py
@@ -1,121 +1,3 @@
-<<<<<<< HEAD
-from math import *
-from soccersimulator import *
-
-def getOpponentGoal(id_team):
-	if id_team == 1 :
-		return Vector2D(GAME_WIDTH, GAME_HEIGHT/2)
-	else : return Vector2D(0, GAME_HEIGHT/2)
-	
-def getOurGoal(id_team):
-	if id_team == 1 :
-		return Vector2D(0, GAME_HEIGHT/2)
-	else : return Vector2D(GAME_WIDTH, GAME_HEIGHT/2)
-	
-def getAngle(vector):
-	return copysign( acos(vector.x/vector.norm), vector.y) 
-	
-	
-def runThere(player, targetedPos):
-	return targetedPos - player.position
-	
-def goThere(player, targetedPos):
-	traj = targetedPos - player.position
-	vitesse = player.vitesse
-	vitesseNormale = vitesse - vitesse.dot(traj / traj.norm) * (traj / traj.norm)
-	if vitesseNormale.norm >= 0 :
-		if vitesseNormale.norm >= maxPlayerAcceleration :
-			return vitesseNormale * -1.
-		else:
-			return vitesseNormale * -1. + traj.normalize() * (maxPlayerAcceleration - vitesseNormale.norm)
-	
-	if getDistanceArret(vitesse.norm) >= traj.norm :
-		return -1. * vitesse
-	
-	return traj
-	
-def getPlayersList(id_team, state):
-	return state.states[id_team]
-	
-def getDistanceArret(vitesse):
-	somme = 0
-	while vitesse >= 0:
-		somme += vitesse
-		vitesse -= maxPlayerAcceleration
-	return somme
-
-	
-	
-class StrategyFonceur(Strategy):
-	def _init_(self, name="Fonceur"):
-		self.name = name
-	
-	def compute_strategy(self, state, id_team, id_player):
-		goal = getOpponentGoal(id_team)
-		
-		acceleration = 1
-		
-		posPlayer = state.player_state(id_team, id_player).position
-		posBall = state.ball.position
-		
-		force = 3.5
-
-		move = runThere( state.player_state(id_team, id_player), posBall)
-
-		shoot = goal - posPlayer
-		shoot.normalize()
-
-		return SoccerAction(move * acceleration, shoot * force)
-		
-class StrategyFonceurBis(Strategy):
-	def _init_(self, name="Fonceur"):
-		self.name = name
-	
-	def compute_strategy(self, state, id_team, id_player):
-		goal = getOpponentGoal(id_team)
-		
-		acceleration = 1
-		
-		posBall = state.ball.position
-		
-		force = 3.5
-
-		move = goThere( state.player_state(id_team, id_player), posBall)
-
-		shoot = goal - state.player_state(id_team, id_player).position
-		shoot.normalize()
-
-		return SoccerAction(move * acceleration, shoot * force)
-
-		
-class StrategyGoal(Strategy):
-	def _init_(self, name="Defenseur"):
-		self.name = name
-
-	def compute_strategy(self, state, id_team, id_player):
-		ourGoal = getOurGoal(id_team)
-		
-		posBall = state.ball.position
-		
-		goalTop = ourGoal + Vector2D(0,GAME_GOAL_HEIGHT/2.)
-		goalBot = ourGoal - Vector2D(0,GAME_GOAL_HEIGHT/2.)
-		
-		ballToGoalTop = goalTop - posBall
-		ballToGoalBot = goalBot - posBall
-		
-		angle = (getAngle(ballToGoalTop) + getAngle(ballToGoalBot)) / 2.
-		
-		pos = posBall + Vector2D(angle = angle, norm = posBall.distance(ourGoal) / 2.3)
-		
-		move = goThere(state.player_state(id_team, id_player), (ballToGoalTop + ballToGoalBot) / 4 + posBall)
-		
-		return SoccerAction(move, Vector2D(0,0))
-
-
-		#ajouter l'angle / 2 puis (distance entre but et balle) /2 + posBut
-
-		
-=======
 from math import *
 from soccersimulator import *
 
@@ -156,18 +38,20 @@ def isClosest(player, opponent, posBall):
 def getPlayersList(id_team, state):
 	return state.states[id_team]
 
-def getOthersPlayers(player, id_team, state):
-	return [ ( it , ip ) for ( it , ip ) in state.players if it != id_team or ip != id_player]	
+def getOthersPlayers(id_player, id_team, state):
+	return [ state.player_state( it , ip ) for ( it , ip ) in state.players if it != id_team or ip != id_player]	
 
-def isBallOwner(player, id_team, state):
-	posBall = state.ball.position
+def isBallOwner(id_player, id_team, state):
+	posBall = state.ball.position + state.ball.vitesse
+	player = state.player_state(id_team, id_player)
 	distanceBallPlayer = (posBall - player.position).norm
-	playerList = getOthersPlayers(player, id_team, state)
+	playerList = getOthersPlayers(id_player, id_team, state)
 	for otherPlayer in playerList : 
 		if (posBall - otherPlayer.position).norm <= distanceBallPlayer:
 			return False
 	return True
 	
+def getBallDistanceArret(vitesse):
 	
 	
 def getDistanceArret(vitesse):
@@ -175,7 +59,7 @@ def getDistanceArret(vitesse):
 		return 0
 
 	somme = 0
-	while vitesse >= 0:
+	while vitesse >= 0: 
 		somme += vitesse
 		vitesse -= maxPlayerAcceleration
 	return somme
@@ -192,7 +76,7 @@ class StrategyFonceur(Strategy):
 		acceleration = 1
 		
 		posPlayer = state.player_state(id_team, id_player).position
-		posBall = state.ball.position
+		posBall = state.ball.position + state.ball.vitesse
 		
 		force = 3.5
 
@@ -212,7 +96,7 @@ class StrategyFonceurBis(Strategy):
 		
 		acceleration = 1
 		
-		posBall = state.ball.position
+		posBall = state.ball.position + state.ball.vitesse
 		
 		force = 3.5
 
@@ -229,8 +113,18 @@ class StrategyGoal(Strategy):
 		self.name = name
 
 	def compute_strategy(self, state, id_team, id_player):
+	
+		if isBallOwner(id_player, id_team, state) :
+			goal = getOpponentGoal(id_team)
+			acceleration = 1
+			force = 5
+			posBall = state.ball.position + state.ball.vitesse
+			move = goThere( state.player_state(id_team, id_player), posBall)
+			shoot = goal - state.player_state(id_team, id_player).position
+			shoot.normalize()
+			return SoccerAction(move * acceleration, shoot * force)
+			
 		ourGoal = getOurGoal(id_team)
-		
 		posBall = state.ball.position
 		
 		goalTop = ourGoal + Vector2D(0,GAME_GOAL_HEIGHT/2.)
@@ -240,7 +134,7 @@ class StrategyGoal(Strategy):
 		ballToGoalBot = goalBot - posBall
 		print("Joueur ",id_team," : ", ballToGoalTop.angle)
 		angle = (ballToGoalTop.angle + ballToGoalBot.angle)/2.
-		vect = Vector2D(norm = ballToGoalTop.x/2., angle = angle)
+		vect = Vector2D(norm = 1./cos(angle)*ballToGoalBot.x/2., angle = angle)
 		
 		move = goThere(state.player_state(id_team, id_player), vect+posBall)
 		
@@ -248,6 +142,3 @@ class StrategyGoal(Strategy):
 
 
 		#ajouter l'angle / 2 puis (distance entre but et balle) /2 + posBut
-
-		
->>>>>>> 5f749532cfde274d465200f4e74269f5f3603a87
--- monProgramme.py
+++ monProgramme.py
@@ -5,7 +5,7 @@ from mesStrategies import *
 gaul1 = Player("Gaul 1", StrategyGoal())
 fonceur1 = Player("Fonceur 1 bis", StrategyFonceurBis())
 team1 = SoccerTeam("equipe 1", [fonceur1, gaul1])
-gaul2 = Player("Gaul 2", StrategyGoal())
+gaul2 = Player("Gaul 2", StrategyFonceur())
 fonceur2 = Player("Fonceur 2", StrategyFonceur())
 team2 = SoccerTeam("equipe 1", [gaul2, fonceur2])
 match = Simulation(team1, team2, 2000)
