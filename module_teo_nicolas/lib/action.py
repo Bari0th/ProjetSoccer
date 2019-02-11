f51ed1dcf4a0aba1b18a2e396e4737bf74dfa186
Nikow <nicolas.ruche.entreprise@gmail.com>
2019-02-11 16:58:24 +0100

    added little actions

--- module_teo_nicolas/lib/action.py
+++ module_teo_nicolas/lib/action.py
@@ -1,33 +1,33 @@
 import soccersimulator as soc
 
-class Move(object):
-	def __init__(self,superstate):
+class Action:
+	def __init__(self, superstate):
 		self.superstate = superstate
+	def computeAction(self):
+		return soc.SoccerAction()
 
-	@property
-	def run_to_ball(self):
+class Move(Action):
+	pass
+
+class Shoot(Action):
+	pass
+
+class RunToBall(Move):
+	def computeAction(self):
 		return soc.SoccerAction( acceleration = self.superstate.vect_play_ball)
 
-	@property
-	def run_to_predict_ball(self):
+class RunToPredictBall(Move):
+	def computeAction(self):
 		return soc.SoccerAction( acceleration = (self.superstate.vect_play_ball + self.superstate.ball_vit * 5 * self.superstate.coeff_distance) )
 
-	@property
-	def run_to_defensive_pos(self):
+class RunToDefensivePos(Move):
+	def computeAction(self):
 		return soc.SoccerAction(acceleration = (self.superstate.defensive_pos + self.superstate.ball_pos - self.superstate.player_pos))
 
-
-class Shoot(object):
-	def __init__(self,superstate):
-		self.superstate = superstate
-
-	@property
-	def shoot_to_goal(self):
-		"""
-		Plus la peine de s'embeter avec le can shoot
-		"""
+class ShootToGoal(Shoot):
+	def computeAction(self):
 		return soc.SoccerAction( shoot = ((self.superstate.opp_goal - self.superstate.player_pos).normalize() * 3) )
 
-	@property
-	def shoot_to_nearest_ally(self):
+class ShootToNearestAlly(Shoot):
+	def computeAction(self):
 		return soc.SoccerAction( shoot = (self.superstate.nearest_ally.position - self.superstate.player_pos).normalize() * 6)
