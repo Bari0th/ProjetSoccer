9444358697552c6cf0a437c7ceb9178bef2f8583
Nikow <nicolas.ruche.entreprise@gmail.com>
2019-02-11 17:11:34 +0100

    removed superstate

--- module_teo_nicolas/lib/action.py
+++ module_teo_nicolas/lib/action.py
@@ -1,47 +1,46 @@
 import soccersimulator as soc
 
 class Action:
-	def __init__(self, superstate, name):
-		self.superstate = superstate
+	def __init__(self, name):
 		self.name = name
-	def computeAction(self):
+	def computeAction(self, superstate):
 		return soc.SoccerAction()
 
 class Move(Action):
-	def __init__(self, superstate, name):
-		Action.__init__(self, superstate, name)
+	def __init__(self, name):
+		Action.__init__(self, name)
 
 class Shoot(Action):
-	def __init__(self, superstate, name):
-		Action.__init__(self, superstate, name)
+	def __init__(self, name):
+		Action.__init__(self, name)
 
 class RunToBall(Move):
-	def __init__(self, superstate):
-		Move.__init__(self, superstate, "RunToBall")
+	def __init__(self):
+		Move.__init__(self, "RunToBall")
 
-	def computeAction(self):
-		return soc.SoccerAction( acceleration = self.superstate.vect_play_ball)
+	def computeAction(self, superstate):
+		return soc.SoccerAction( acceleration = superstate.vect_play_ball)
 
 class RunToPredictBall(Move):
-	def __init__(self, superstate):
-		Move.__init__(self, superstate, "RunToPredictBall")
-	def computeAction(self):
-		return soc.SoccerAction( acceleration = (self.superstate.vect_play_ball + self.superstate.ball_vit * 5 * self.superstate.coeff_distance) )
+	def __init__(self):
+		Move.__init__(self, "RunToPredictBall")
+	def computeAction(self, superstate):
+		return soc.SoccerAction( acceleration = (superstate.vect_play_ball + superstate.ball_vit * 5 * superstate.coeff_distance) )
 
 class RunToDefensivePos(Move):
-	def __init__(self, superstate):
-		Move.__init__(self, superstate, "RunToDefensivePos")
-	def computeAction(self):
-		return soc.SoccerAction(acceleration = (self.superstate.defensive_pos + self.superstate.ball_pos - self.superstate.player_pos))
+	def __init__(self):
+		Move.__init__(self, "RunToDefensivePos")
+	def computeAction(self, superstate):
+		return soc.SoccerAction(acceleration = (superstate.defensive_pos + superstate.ball_pos - superstate.player_pos))
 
 class ShootToGoal(Shoot):
-	def __init__(self, superstate):
-		Shoot.__init__(self, superstate, "ShootToGoal")
-	def computeAction(self):
-		return soc.SoccerAction( shoot = ((self.superstate.opp_goal - self.superstate.player_pos).normalize() * 3) )
+	def __init__(self):
+		Shoot.__init__(self, "ShootToGoal")
+	def computeAction(self, superstate):
+		return soc.SoccerAction( shoot = ((superstate.opp_goal - superstate.player_pos).normalize() * 3) )
 
 class ShootToNearestAlly(Shoot):
-	def __init__(self, superstate):
-		Shoot.__init__(self, superstate, "ShootToNearestAlly")
-	def computeAction(self):
-		return soc.SoccerAction( shoot = (self.superstate.nearest_ally.position - self.superstate.player_pos).normalize() * 6)
+	def __init__(self):
+		Shoot.__init__(self, "ShootToNearestAlly")
+	def computeAction(self, superstate):
+		return soc.SoccerAction( shoot = (superstate.nearest_ally.position - superstate.player_pos).normalize() * 6)
