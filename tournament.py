7ca6b594a1da32fca5fbd2ed8158f97bf95842a2
Téo Bernier <3670574@ppti-14-303-17.ufr-info-p6.jussieu.fr>
2019-02-11 20:06:58 +0100

    tournament.py

---
+++ tournament.py
@@ -0,0 +1,24 @@
+from soccersimulator import SoccerTeam
+from module_teo_nicolas import action
+from module_teo_nicolas import strategy_encapsulator
+
+def get_team ( nb_players ):
+	team = SoccerTeam ( name = " Nicolas / Téo ")
+	if nb_players == 1:
+		team.add ( " goal " , SimpleStrategy(GoalBehavior()))
+	if nb_players == 2:
+		team.add ( " fonceur bis" , SimpleStrategy(FonceurBehavior()))
+		team.add ( " gaul " , SimpleStrategy(GoalBehavior()))
+	return team
+
+if __name__ == '__main__':
+	team1 = get_team(1)
+	team2 = SoccerTeam("equipe 1", [Player("FonceurBis", FonceurBehavior())])
+	match = Simulation(team1, team2, 2000)
+	match.start()
+	show_simu(match)
+	team1 = get_team(2)
+	team2 = SoccerTeam("equipe 1", [Player("Fonceur", FonceurBehavior()), Player("FonceurBis", FonceurBehavior())])
+	match = Simulation(team1, team2, 2000)
+	match.start()
+	show_simu(match)
