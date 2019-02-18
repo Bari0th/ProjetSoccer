f14313a0f5f8fdd03b79a7de73e9b98e977630d6
Nikow <nicolas.ruche.entreprise@gmail.com>
2019-02-18 12:21:24 +0100

    changed modules for tournament.py

--- tournament.py
+++ tournament.py
@@ -1,15 +1,6 @@
 from module_teo_nicolas import strategies as strat
 import soccersimulator as soc
-
-
-def get_team ( nb_players ):
-	team = soc.SoccerTeam ( name = " Nicolas / Téo ")
-	if nb_players == 1:
-		team.add ( " Goal " , strat.createStrategy(strat.GoalBehavior()))
-	if nb_players == 2:
-		team.add ( " Attaquant " , strat.createStrategy(strat.FonceurBehavior()))
-		team.add ( " Gaul " , strat.createStrategy(strat.GoalBehavior()))
-	return team
+from module_teo_nicolas import get_team
 
 if __name__ == '__main__':
 	team1 = get_team(1)
