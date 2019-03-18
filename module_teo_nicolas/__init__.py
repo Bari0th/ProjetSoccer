b3b43ba13f9ce258622b8586c0a854d05c952dbc
Nikow <nicolas.ruche.entreprise@gmail.com>
2019-03-18 22:32:16 +0100

    working changes

--- module_teo_nicolas/__init__.py
+++ module_teo_nicolas/__init__.py
@@ -1,11 +1,11 @@
-from .lib.strategies import AutoBehavior, createStrategy
+from .lib.strategies import GoalBehaviorAlone, createStrategy, GoalBehaviorTeam, AttaquantBehavior
 import soccersimulator as soc
 
 def get_team ( nb_players ):
-	team = soc.SoccerTeam ( name = " Nicolas / Téo ")
+	team = soc.SoccerTeam ( name = " Inazuma {} ".format(nb_players))
 	if nb_players == 1:
-		team.add ( " Auto " , createStrategy(AutoBehavior()))
+		team.add ( " Lone Wolf " , createStrategy(GoalBehaviorAlone()))
 	if nb_players == 2:
-		team.add ( " Auto 1 " , createStrategy(AutoBehavior()))
-		team.add ( " Auto 2 " , createStrategy(AutoBehavior()))
+		team.add ( " SNK " , createStrategy(AttaquantBehavior()))
+		team.add ( " Mark Evans " , createStrategy(GoalBehaviorTeam()))
 	return team
