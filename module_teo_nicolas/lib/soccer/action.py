462aca9d1a66e7e2d602bfb9400c910bf6ca29fa
Téo Bernier <3670574@ppti-14-303-17.ufr-info-p6.jussieu.fr>
2019-03-25 17:00:43 +0100

    new branch

--- module_teo_nicolas/lib/soccer/action.py
+++ module_teo_nicolas/lib/soccer/action.py
@@ -1,29 +1,41 @@
 import soccersimulator as soc
 import math
 
-def getAllActions():
+def getAllActions(nbplayers):
 	actions = dict()
 
-	moves_classes = Move.__subclasses__()
-	tuples = list(map(lambda x : (x.__name__, x), moves_classes))
-	moves = {'names' : []}
+	qmoves_classes = QMoves.__subclasses__()
+	tuples = list(map(lambda x : (x.__name__, x), qmoves_classes))
+	qmoves = {'names' : []}
 
-	for name, move_class in tuples :
-		moves["names"].append(name)
-		moves[name] = move_class
-
-
-	shoots_classes = Shoot.__subclasses__()
-	tuples = list(map(lambda x : (x.__name__, x), shoots_classes))
-	shoots = {'names' : []}
-
-	for name, shoot_class in tuples :
-		shoots["names"].append(name)
-		shoots[name] = shoot_class
+	for name, qmove_class in tuples :
+		qmoves["names"].append(name)
+		qmoves[name] = qmove_class
 
 
-	actions["moves"] = moves
-	actions["shoots"] = shoots
+	qshoots_classes = QShoots.__subclasses__()
+	tuples = list(map(lambda x : (x.__name__, x), qshoots_classes))
+	qshoots = {'names' : []}
+
+	for name, qshoot_class in tuples :
+		qshoots["names"].append(name)
+		qshoots[name] = qshoot_class
+
+
+
+
+	if nbplayers > 1:
+		
+		qshootsteam_classes = QShootsTeam.__subclasses__()
+		tuples = list(map(lambda x : (x.__name__, x), qshootsteam_classes))
+
+		for name, qshootsteam_class in tuples :
+			qshoots["names"].append(name)
+			qshoots[name] = qshootsteam_class
+
+
+	actions["moves"] = qmoves
+	actions["shoots"] = qshoots
 	return actions
 
 class Action:
@@ -39,13 +51,6 @@ class Move(Action):
 class Shoot(Action):
 	def __init__(self, name):
 		Action.__init__(self, name)
-
-class DontMove(Move):
-	def __init__(self):
-		Move.__init__(self, "DontMove")
-
-	def computeAction(self, superstate):
-		return soc.SoccerAction()
 
 class RunToBall(Move):
 	def __init__(self):
@@ -71,12 +76,6 @@ class RunToCloseDefensivePos(Move):
 		Move.__init__(self, "RunToDefensivePos")
 	def computeAction(self, superstate):
 		return soc.SoccerAction(acceleration = (superstate.close_defensive_pos + superstate.ball_pos - superstate.player_pos))
-class DontShoot(Shoot):
-	def __init__(self):
-		Shoot.__init__(self, "DontShoot")
-
-	def computeAction(self, superstate):
-		return soc.SoccerAction()
 
 class ShootToGoal(Shoot):
 	def __init__(self):
@@ -121,5 +120,88 @@ class ShootToCornerFarFromOpp(Shoot):
 		shoot = (superstate.ally_corner_far_opp_near_player - superstate.player_pos).normalize() * 4
 		return soc.SoccerAction( shoot = shoot)
 
+class QMoves(Move):
+	def __init__(self, name):
+		Action.__init__(self, name)
+
+class RunToBall(QMoves):
+	def __init__(self):
+		Move.__init__(self, "RunToBall")
+
+	def computeAction(self, superstate):
+		return soc.SoccerAction( acceleration = superstate.vect_play_ball)
+
+class RunToPredictBall(QMoves):
+	def __init__(self):
+		Move.__init__(self, "RunToPredictBall")
+	def computeAction(self, superstate):
+		return soc.SoccerAction( acceleration = (superstate.vect_play_ball + superstate.ball_vit * 20 * superstate.coeff_distance) )
+
+class RunToDefensivePos(QMoves):
+	def __init__(self):
+		Move.__init__(self, "RunToDefensivePos")
+	def computeAction(self, superstate):
+		return soc.SoccerAction(acceleration = (superstate.defensive_pos + superstate.ball_pos - superstate.player_pos))
+
+class RunToCloseDefensivePos(QMoves):
+	def __init__(self):
+		Move.__init__(self, "RunToDefensivePos")
+	def computeAction(self, superstate):
+		return soc.SoccerAction(acceleration = (superstate.close_defensive_pos + superstate.ball_pos - superstate.player_pos))
+
+
+class QShoots(Shoot):
+	def __init__(self, name):
+		Action.__init__(self, name)
+
+class QShootsTeam(Shoot):
+	def __init__(self, name):
+		Action.__init__(self, name)
+
+
+class ShootToGoal(QShoots):
+	def __init__(self):
+		Shoot.__init__(self, "ShootToGoal")
+	def computeAction(self, superstate):
+		return soc.SoccerAction( shoot = ((superstate.opp_goal - superstate.player_pos).normalize() * 6) )
+
+class ShootToNearestAlly(QShootsTeam):
+	def __init__(self):
+		Shoot.__init__(self, "ShootToNearestAlly")
+	def computeAction(self, superstate):
+		return soc.SoccerAction( shoot = (superstate.nearest_ally.position - superstate.player_pos).normalize() * 6)
+
+class ShootToNearestAllyFarFromOpponent(QShootsTeam):
+	def __init__(self):
+		Shoot.__init__(self, "ShootAllyFarOpp")
+	def computeAction(self, superstate):
+		shoot = (superstate.nearest_ally.position - superstate.player_pos).normalize() * 6
+		shoot.angle += (math.pi / 12 * math.copysign(1, shoot.angle - (superstate.nearest_opp.position - superstate.player_pos).angle))
+		return soc.SoccerAction( shoot = shoot )
+			
+
+class ShootToMoveToGoal(QShoots):
+	def __init__(self):
+		Shoot.__init__(self, "ShootToMove")
+	def computeAction(self,superstate):
+		return soc.SoccerAction(shoot = (superstate.opp_goal - superstate.player_pos).normalize() * 1.5)
+	
+class StrongShootToGoal(QShoots):
+	def __init__(self):
+		Shoot.__init__(self, "StrongShootToGoal")
+	def computeAction(self,superstate):
+		if (superstate.player_pos.distance(superstate.ball_pos) < 20):
+			coeff = (40 - (superstate.player_pos - superstate.ball_pos).norm)/20 * 3
+		else : coeff = 3
+		return soc.SoccerAction(shoot = ((superstate.opp_goal - superstate.player_pos).normalize() * coeff))
+
+class ShootToCornerFarFromOpp(QShoots):
+	def __init__(self):
+		Shoot.__init__(self, "ShootToCorner")
+	def computeAction(self,superstate):
+		shoot = (superstate.ally_corner_far_opp_near_player - superstate.player_pos).normalize() * 4
+		return soc.SoccerAction( shoot = shoot)
+
 if __name__ == "__main__":
-	print(getAllActions())
+	print(getAllActions(1))
+	print(getAllActions(2))
