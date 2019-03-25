da43f47de1451a091d96e8130b9e34135f451898
Nikow <nicolas.ruche.entreprise@gmail.com>
2019-03-25 19:23:47 +0100

    added four teams

--- module_teo_nicolas/__init__.py
+++ module_teo_nicolas/__init__.py
@@ -8,4 +8,9 @@ def get_team ( nb_players ):
 	if nb_players == 2:
 		team.add ( " SNK " , createStrategy(AttaquantBehavior()))
 		team.add ( " Mark Evans " , createStrategy(GoalBehaviorTeam()))
+	if nb_players == 4:
+		team.add ( " SNK " , createStrategy(AttaquantBehavior()))
+		team.add ( " Mark Evans " , createStrategy(GoalBehaviorTeam()))
+		team.add ( " Mark Evans " , createStrategy(AttaquantBehavior()))
+		team.add ( " Mark Evans " , createStrategy(AutoBehavior()))
 	return team
--- tournament.py
+++ tournament.py
@@ -2,13 +2,8 @@ import soccersimulator as soc
 from module_teo_nicolas import get_team
 
 if __name__ == '__main__':
-	team1 = get_team(1)
-	team2 = get_team(1)
-	match = soc.Simulation(team1, team2, 2000)
-	match.start()
-	soc.show_simu(match)
-	team1 = get_team(2)
-	team2 = get_team(2)
+	team1 = get_team(4)
+	team2 = get_team(4)
 	match = soc.Simulation(team1, team2, 2000)
 	match.start()
 	soc.show_simu(match)
