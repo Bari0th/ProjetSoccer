5f66e5257e0e7e536b79e4e05627d94aecfb7ab0
Nikow <nicolas.ruche.entreprise@gmail.com>
2019-04-01 19:44:17 +0200

    nulle team 4

--- module_teo_nicolas/__init__.py
+++ module_teo_nicolas/__init__.py
@@ -4,13 +4,23 @@ import soccersimulator as soc
 def get_team ( nb_players ):
 	team = soc.SoccerTeam ( name = " Inazuma {} ".format(nb_players))
 	if nb_players == 1:
-		team.add ( " Lone Wolf " , createStrategy(GoalBehaviorAlone()))
+		team.add ( " Mark Evans " , createStrategy(GoalBehaviorAlone()))
 	if nb_players == 2:
-		team.add ( " SNK " , createStrategy(AttaquantBehavior()))
+		team.add ( " Axel Blaze " , createStrategy(AttaquantBehavior()))
 		team.add ( " Mark Evans " , createStrategy(newGoalBehaviorTeam()))
 	if nb_players == 4:
-		team.add ( " SNK " , createStrategy(AttaquantBehavior()))
+		team.add ( " Axel Blaze " , createStrategy(AttaquantBehavior()))
 		team.add ( " Mark Evans " , createStrategy(newGoalBehaviorTeam()))
-		team.add ( " Mark Evans " , createStrategy(AttaquantBehavior()))
+		team.add ( " Axel Blaze " , createStrategy(AttaquantBehavior()))
 		team.add ( " Mark Evans " , createStrategy(newGoalBehaviorTeam()))
 	return team
+
+def get_team_opp():
+	team2 = soc.SoccerTeam ( name = " Royal Academy ")
+
+	team2.add ( " Nul " , createStrategy(AttaquantBehavior()))
+	team2.add ( " Nul " , createStrategy(newGoalBehaviorTeam()))
+	team2.add ( " Nul " , createStrategy(AttaquantBehavior()))
+	team2.add ( " Nul " , createStrategy(newGoalBehaviorTeam()))
+
+	return team2
